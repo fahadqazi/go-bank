609e21058b47d9a960ea24fda03eb8af717210c3
Fahad Qazi <fahadqazi@gmail.com>
2023-08-01 08:09:50 +0100

    initial commit

---
+++ api.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			// handle error here
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddr string
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{listenAddr: listenAddr}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+
+	log.Printf("Serving on port: %s", s.listenAddr)
+
+	http.ListenAndServe(s.listenAddr, router)
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == http.MethodGet {
+		return s.handleGetAccount(w, r)
+	}
+
+	if r.Method == http.MethodPost {
+		return s.handleCreateAccount(w, r)
+	}
+
+	if r.Method == http.MethodDelete {
+		return s.handleDeleteAcount(w, r)
+	}
+
+	return fmt.Errorf("methot not allowed: %s", r.Method)
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	account := NewAccount("Fahad", "Qazi")
+	return WriteJSON(w, http.StatusOK, account)
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleDeleteAcount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransferAcount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fahadqazi/go-bank
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
