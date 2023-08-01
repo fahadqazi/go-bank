5ea94c3bd8ef568106a3ef5b9f5b807ff48eb40d
Fahad Qazi <fahadqazi@gmail.com>
2023-08-01 08:13:08 +0100

    fix header order

--- api.go
+++ api.go
@@ -10,8 +10,8 @@ import (
 )
 
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(v)
 }
 
